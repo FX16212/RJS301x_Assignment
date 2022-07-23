32567dc88efdd88cae3159412d74b1fb82762de5
EnZooZ <enzooz4889@gmail.com>
2022-07-23 18:45:40 +0700

    Add detail component

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,9 +1,116 @@
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
+import MailList from '../../components/mailList/MailList';
+import Footer from '../../components/footer/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faCircleArrowLeft,
+	faCircleArrowRight,
+	faCircleXmark,
+	faLocationDot,
+} from '@fortawesome/free-solid-svg-icons';
+import detail from '../../data/detail';
+import { useState } from 'react';
+import './Detail.css';
 const Detail = () => {
-  return (
-    <div>
-      <h1>Detail Page</h1>
-    </div>
-  );
+	// State
+	const [slideNumber, setSlideNumber] = useState(0);
+	const [open, setOpen] = useState(false);
+
+	// Handle open Modal
+	const handleOpen = (i) => {
+		setSlideNumber(i);
+		setOpen(true);
+	};
+	// Hanle Move
+	const handleMove = (direction) => {
+		let newSlideNumber;
+
+		if (direction === 'l') {
+			newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+		} else {
+			newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+		}
+
+		setSlideNumber(newSlideNumber);
+	};
+	return (
+		<div>
+			<Navbar />
+			<Header type="list" />
+			<div className="detail-Container">
+				{open && (
+					<div className="slider">
+						<FontAwesomeIcon
+							icon={faCircleXmark}
+							className="close"
+							onClick={() => setOpen(false)}
+						/>
+						<FontAwesomeIcon
+							icon={faCircleArrowLeft}
+							className="arrow"
+							onClick={() => handleMove('l')}
+						/>
+						<div className="sliderWrapper">
+							<img
+								src={detail.photos[slideNumber]}
+								alt=""
+								className="sliderImg"
+							/>
+						</div>
+						<FontAwesomeIcon
+							icon={faCircleArrowRight}
+							className="arrow"
+							onClick={() => handleMove('r')}
+						/>
+					</div>
+				)}
+				<div className="detail-Wrapper">
+					<button className="bookNow">Reserve or Book Now!</button>
+					<h1 className="detail-Title">{detail.name}</h1>
+					<div className="detail-Address">
+						<FontAwesomeIcon icon={faLocationDot} />
+						<span>{detail.address}</span>
+					</div>
+					<span className="detail-Distance">{detail.distance}</span>
+					<span className="detail-PriceHighlight">{detail.price}</span>
+					<div className="detail-Images">
+						{detail.photos.map((photo, index) => {
+							return (
+								<div className="detail-ImgWrapper" key={index}>
+									<img
+										onClick={() => handleOpen(index)}
+										src={photo}
+										alt=""
+										className="detail-Img"
+									/>
+								</div>
+							);
+						})}
+					</div>
+					<div className="detail-Details">
+						<div className="detail-DetailsTexts">
+							<h1 className="detail-Title">{detail.title}</h1>
+							<p className="detail-Desc">{detail.description}</p>
+						</div>
+						<div className="detail-DetailsPrice">
+							<h1>Perfect for a 9-night stay!</h1>
+							<span>
+								Located in the real heart of Krakow, this property has an
+								excellent location score of 9.8!
+							</span>
+							<h2>
+								<b>$ {detail.nine_night_price}</b> (9 nights)
+							</h2>
+							<button>Reserve or Book Now!</button>
+						</div>
+					</div>
+				</div>
+				<MailList />
+				<Footer />
+			</div>
+		</div>
+	);
 };
 
 export default Detail;
--- src/pages/search/searchList/SearchList.jsx
+++ src/pages/search/searchList/SearchList.jsx
@@ -7,31 +7,33 @@ function SearchList() {
 		<div>
 			{SearchItems.map((item, index) => {
 				return (
-					<div className="hotelSearch" key={index}>
-						<img src={item.image_url} alt="" className="hotelImg" />
-						<div className="hotelDesc">
-							<h1 className="hotelTitle">{item.name}</h1>
-							<span className="hotelDistance">
+					<div className="searchList" key={index}>
+						<img src={item.image_url} alt="" className="searchList-Img" />
+						<div className="searchList-Desc">
+							<h1 className="searchList-Title">{item.name}</h1>
+							<span className="searchList-Distance">
 								{' '}
 								{item.distance} from center
 							</span>
-							<span className="hotelTaxiOp">{item.tag}</span>
-							<span className="hotelSubtitle">{item.description}</span>
-							<span className="hotelFeatures">{item.type}</span>
-							<span className="hotelCancelOp">
+							<span className="searchList-TaxiOp">{item.tag}</span>
+							<span className="searchList-Subtitle">{item.description}</span>
+							<span className="searchList-Features">{item.type}</span>
+							<span className="searchList-CancelOp">
 								{' '}
 								{item.free_cancel ? 'Free cancellation' : ''}{' '}
 							</span>
 						</div>
-						<div className="hotelDetails">
-							<div className="hotelRating">
+						<div className="searchList-Details">
+							<div className="searchList-Rating">
 								<span> {item.rate_text} </span>
 								<button> {item.rate} </button>
 							</div>
-							<div className="hotelDetailTexts">
-								<span className="hotelPrice">$ {item.price} </span>
-								<p className="hotelTaxOp">Includes taxes and fees</p>
-								<button className="hotelCheckButton">See availability</button>
+							<div className="searchList-DetailTexts">
+								<span className="searchList-Price">$ {item.price} </span>
+								<p className="searchList-TaxOp">Includes taxes and fees</p>
+								<button className="searchList-CheckButton">
+									See availability
+								</button>
 							</div>
 						</div>
 					</div>
