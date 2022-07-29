1a05c677a42d2cc830bde164d1cb82425861798e
EnZooZ <enzooz4889@gmail.com>
2022-07-29 20:35:23 +0700

    Add Banner Component

--- src/pages/browse/Browse.jsx
+++ src/pages/browse/Browse.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Browse.css';
 import Navbar from '../../components/Navbar/Navbar';
+import Banner from '../../components/Banner/Banner';
 
 function Browse() {
 	return (
-		<>
+		<div className="app">
 			<Navbar />
-		</>
+			<Banner />
+		</div>
 	);
 }
 
