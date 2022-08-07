2cf19e5fd6e461c19b1a86927ce875b4bf0eca2d
EnZooZ <enzooz4889@gmail.com>
2022-08-07 21:08:44 +0700

    Tạo Router cho ứng dụng

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './pages/cart/Cart';
+import Checkout from './pages/checkout/Checkout';
+import Detail from './pages/detail/Detail';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register,';
+import Shop from './pages/shop/Shop';
+
 function App() {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/shop" element={<Shop />} />
+				<Route path="/detail/:id" element={<Detail />} />
+				<Route path="/cart" element={<Cart />} />
+				<Route path="/checkout" element={<Checkout />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
